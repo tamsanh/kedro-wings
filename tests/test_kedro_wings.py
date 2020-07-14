715a32ad927ea7f7320e6498c1be862e8e5544b9
Tam Nguyen <Tam_Nguyen@McKinsey.com>
2020-07-15 04:54:54 +0800

    Reformat with black

--- tests/test_kedro_wings.py
+++ tests/test_kedro_wings.py
@@ -12,18 +12,22 @@ def test_wing_info_to_config():
 
     from kedro.extras.datasets import pandas
 
-    datasets = {
-        '.custom': pandas.CSVDataSet,
-        '.missing_type': {'invalid': 'dataset'}
-    }
+    datasets = {".custom": pandas.CSVDataSet, ".missing_type": {"invalid": "dataset"}}
 
     wings = KedroWings(datasets)
-    info = WingInfo(directory='dir', name='data', extension='.custom', basename='data.custom')
+    info = WingInfo(
+        directory="dir", name="data", extension=".custom", basename="data.custom"
+    )
     config = wings._wing_to_dataset_config(info)
-    assert config == {'type': pandas.CSVDataSet, 'filepath': 'data/dir/data.custom'}
+    assert config == {"type": pandas.CSVDataSet, "filepath": "data/dir/data.custom"}
 
     with pytest.raises(MissingType):
-        info = WingInfo(directory='dir', name='data', extension='.missing_type', basename='data.missing_type')
+        info = WingInfo(
+            directory="dir",
+            name="data",
+            extension=".missing_type",
+            basename="data.missing_type",
+        )
         wings._wing_to_dataset_config(info)
 
 
@@ -37,7 +41,17 @@ def test_kedro_wings_and_context():
 
         @property
         def pipelines(self):
-            return {'hi': Pipeline([node(lambda x: x, inputs='01_raw/data.csv', outputs='02_intermediate/data.csv')])}
+            return {
+                "hi": Pipeline(
+                    [
+                        node(
+                            lambda x: x,
+                            inputs="01_raw/data.csv",
+                            outputs="02_intermediate/data.csv",
+                        )
+                    ]
+                )
+            }
 
         @property
         def catalog(self):
@@ -46,69 +60,80 @@ def test_kedro_wings_and_context():
     mock_context = FakeContext()
 
     KedroWings(context=mock_context)
-    assert set(mock_context.catalog.list()) == {'01_raw/data.csv', '02_intermediate/data.csv'}
+    assert set(mock_context.catalog.list()) == {
+        "01_raw/data.csv",
+        "02_intermediate/data.csv",
+    }
 
 
 def test_create_chrono_entries():
     wings = KedroWings()
 
-    catalog_name = 'data!'
+    catalog_name = "data!"
     from kedro.extras.datasets.pandas import CSVDataSet
-    new_dataset = CSVDataSet('fake_path')
-    catalog_datasets = {'data': new_dataset}
-    catalog_entry = wings._create_chronocode_entries([catalog_name], catalog_datasets)[catalog_name]
+
+    new_dataset = CSVDataSet("fake_path")
+    catalog_datasets = {"data": new_dataset}
+    catalog_entry = wings._create_chronocode_entries([catalog_name], catalog_datasets)[
+        catalog_name
+    ]
     assert new_dataset == catalog_entry
 
     with pytest.raises(MissingChronoDataSetTarget):
-        catalog_name = 'data!'
+        catalog_name = "data!"
         wings._create_chronocode_entries([catalog_name], {})
 
 
 def test_create_entries():
     wings = KedroWings()
 
-    catalog_name = '01_raw/data.csv'
-    catalog_names = [catalog_name, f'{catalog_name}!']
+    catalog_name = "01_raw/data.csv"
+    catalog_names = [catalog_name, f"{catalog_name}!"]
     all_entries = wings._create_entries(catalog_names, {})
     assert len(all_entries) == 2
-    assert set(all_entries.keys()) == {'01_raw/data.csv', '01_raw/data.csv!'}
-    print([str(x._filepath) == 'data/01_raw/data.csv' for x in all_entries.values()])
+    assert set(all_entries.keys()) == {"01_raw/data.csv", "01_raw/data.csv!"}
+    print([str(x._filepath) == "data/01_raw/data.csv" for x in all_entries.values()])
 
 
 def test_create_wing_entries():
     wings = KedroWings()
 
-    catalog_name = '01_raw/data.csv'
+    catalog_name = "01_raw/data.csv"
     catalog_entry = wings._create_wing_entries([catalog_name])[catalog_name]
 
     from kedro.extras.datasets.pandas import CSVDataSet
-    assert isinstance(catalog_entry, CSVDataSet)
-    assert str(catalog_entry._filepath) == os.path.join('data', catalog_name)
 
-    catalog_name = '02_intermediate/data.png'
+    assert isinstance(catalog_entry, CSVDataSet)
+    assert str(catalog_entry._filepath) == os.path.join("data", catalog_name)
+
+    catalog_name = "02_intermediate/data.png"
     catalog_entry = wings._create_wing_entries([catalog_name])[catalog_name]
 
     from kedro.extras.datasets.pillow import ImageDataSet
-    assert isinstance(catalog_entry, ImageDataSet)
-    assert str(catalog_entry._filepath) == os.path.join('data', catalog_name)
 
-    catalog_name = '02_intermediate/data.yml'
+    assert isinstance(catalog_entry, ImageDataSet)
+    assert str(catalog_entry._filepath) == os.path.join("data", catalog_name)
+
+    catalog_name = "02_intermediate/data.yml"
     catalog_entry = wings._create_wing_entries([catalog_name])[catalog_name]
 
     from kedro.extras.datasets.yaml import YAMLDataSet
-    assert isinstance(catalog_entry, YAMLDataSet)
-    assert str(catalog_entry._filepath) == os.path.join('data', catalog_name)
 
-    catalog_name = '02_intermediate/data.xls'
+    assert isinstance(catalog_entry, YAMLDataSet)
+    assert str(catalog_entry._filepath) == os.path.join("data", catalog_name)
+
+    catalog_name = "02_intermediate/data.xls"
     catalog_entry = wings._create_wing_entries([catalog_name])[catalog_name]
 
     from kedro.extras.datasets.pandas import ExcelDataSet
-    assert isinstance(catalog_entry, ExcelDataSet)
-    assert str(catalog_entry._filepath) == os.path.join('data', catalog_name)
 
-    catalog_name = '02_intermediate/data.pkl'
+    assert isinstance(catalog_entry, ExcelDataSet)
+    assert str(catalog_entry._filepath) == os.path.join("data", catalog_name)
+
+    catalog_name = "02_intermediate/data.pkl"
     catalog_entry = wings._create_wing_entries([catalog_name])[catalog_name]
 
     from kedro.extras.datasets.pickle import PickleDataSet
+
     assert isinstance(catalog_entry, PickleDataSet)
-    assert str(catalog_entry._filepath) == os.path.join('data', catalog_name)
+    assert str(catalog_entry._filepath) == os.path.join("data", catalog_name)
--- tests/test_wing_info.py
+++ tests/test_wing_info.py
@@ -10,28 +10,32 @@ __version__ = "0.1.0"
 @pytest.fixture
 def valid_catalog_names():
     return [
-        '01_raw/sample_data.csv',
-        '02_intermediate/sample_data.txt',
-        '03_primary/sample_data.png',
+        "01_raw/sample_data.csv",
+        "02_intermediate/sample_data.txt",
+        "03_primary/sample_data.png",
     ]
 
 
 @pytest.fixture
 def valid_extensions():
-    return {'.csv', '.txt', '.png'}
+    return {".csv", ".txt", ".png"}
 
 
 @pytest.fixture
 def valid_wings_datasets():
     return [
-        WingInfo('01_raw', 'sample_data', '.csv', 'sample_data.csv'),
-        WingInfo('02_intermediate', 'sample_data', '.txt', 'sample_data.txt'),
-        WingInfo('03_primary', 'sample_data', '.png', 'sample_data.png'),
+        WingInfo("01_raw", "sample_data", ".csv", "sample_data.csv"),
+        WingInfo("02_intermediate", "sample_data", ".txt", "sample_data.txt"),
+        WingInfo("03_primary", "sample_data", ".png", "sample_data.png"),
     ]
 
 
-def test_parse_valid_entries(valid_catalog_names, valid_extensions, valid_wings_datasets):
-    for valid_catalog_name, valid_wings_dataset in zip(valid_catalog_names, valid_wings_datasets):
+def test_parse_valid_entries(
+    valid_catalog_names, valid_extensions, valid_wings_datasets
+):
+    for valid_catalog_name, valid_wings_dataset in zip(
+        valid_catalog_names, valid_wings_datasets
+    ):
         parsed_wings_dataset = parse_wing_info(valid_catalog_name, valid_extensions)
         assert parsed_wings_dataset == valid_wings_dataset
 
@@ -42,14 +46,15 @@ def test_parse_invalid_entries(valid_catalog_names):
 
 
 def test_prioritize_multiple_extensions():
-    extensions = {
-        '.html', '.profile.html', '.csv', '.true.csv'
-    }
+    extensions = {".html", ".profile.html", ".csv", ".true.csv"}
     valid_catalog_names = [
-        '01_raw/test.html',
-        '01_raw/test.profile.html',
-        '01_raw/test.true.csv',
+        "01_raw/test.html",
+        "01_raw/test.profile.html",
+        "01_raw/test.true.csv",
     ]
     for valid_catalog_name in valid_catalog_names:
         parsed_wing = parse_wing_info(valid_catalog_name, extensions)
-        assert os.path.join(parsed_wing.directory, parsed_wing.basename) == valid_catalog_name
+        assert (
+            os.path.join(parsed_wing.directory, parsed_wing.basename)
+            == valid_catalog_name
+        )
